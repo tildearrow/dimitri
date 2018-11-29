373cd86487508d2311f2eb60e92ad88e2824dee0
tildearrow <tildearrow@users.noreply.github.com>
2018-11-29 15:24:08 -0500

    static green
    i'll begin writing the actual stuff in a few hours

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include "libopenrazer.h"
+#include <unistd.h>
 #include <QColor>
 #include <QDebug>
 
+int frame;
+
 int main(int argc, char** argv) {
+  frame=0;
   if (!libopenrazer::isDaemonRunning()) {
     printf("OpenRazer daemon not running!\n");
     return 1;
@@ -13,14 +17,21 @@ int main(int argc, char** argv) {
     libopenrazer::Device device=libopenrazer::Device(str);
     printf("name: %s\n",qPrintable(device.getDeviceName()));
     printf("serial: %s\n",qPrintable(str));
+    QVector<QColor> colors=QVector<QColor>();
 
     if (device.hasMatrix()) {
       QList<int> dimen=device.getMatrixDimensions();
-      QVector<QColor> colors=QVector<QColor>();
-      for (int i=0; i<dimen[1]; i++) colors << QColor(0,i*6,128-i*4);
-      for (int i=0; i<dimen[0]; i++) {
-        device.setKeyRow(i, 0, dimen[1]-1, colors);
+      for (int i=0; i<dimen[1]; i++) colors<<QColor(0,0,0);
+      while (1) {
+        printf("frame %d\n",frame);
+        for (int i=0; i<dimen[0]; i++) {
+          //for (int i=0; i<dimen[1]; i++) colors[i]=QColor(0,((frame+i*2)*16)%255,128-i*4);
+          for (int i=0; i<dimen[1]; i++) colors[i]=QColor(0,255,0);
+          device.setKeyRow(i, 0, dimen[1]-1, colors);
+        }
         device.setCustom();
+        frame++;
+        usleep(30000);
       }
     }
   }
